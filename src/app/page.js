d3043548995de9364daa3e93ee2e3b21b5dad8ce
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2023-10-08 00:36:47 +0600

    complete this task

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
-import Image from "next/image";
-import bg from "../assets/bg.jpg";
+import Link from "next/link";
+import { FaUsers } from "react-icons/fa";
+import { BiChevronRight } from "react-icons/bi";
+import { FaUserGraduate } from "react-icons/fa";
+import { GiOpenBook } from "react-icons/gi";
 
 export default function Home() {
   return (
@@ -8,59 +11,81 @@ export default function Home() {
         <div className="banner__container w-full h-screen bg-[linear-gradient(135deg,_rgba(13,16,40,1)_0%,_rgba(13,16,40,0.6)_35%,_rgba(255,82,2,0.8)_100%)]">
           <div className="container w-full h-full flex items-center justify-center">
             <div className="banner__content w-2/3 text-center mx-auto">
-              {/* <Image src={bg} alt="img" height={300} width={500} /> */}
-              <h1 className="font-bold text-[84px] capitalize text-white ">
+              <h1 className="font-roboto font-bold text-[24px] sm:text-[32px] md:text-[48px] lg:text-[64px] xl:text-[84px] capitalize text-white ">
                 better education for a better world.
               </h1>
-              <p className="px-44 text-lg text-white">
+              <p className=" font-roboto text-sm sm:text-base lg:text-lg lg:px-20 xl:px-40 text-white">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                pariatur cupiditate tenetur aperiam. Sed, alias!
+                pariatur cupiditate tenetur aperiam. Sed, alias! Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Eligendi, aliquid?
               </p>
-              <button className="uppercase text-xl mt-10 px-10 py-5 bg-[#FF5202] transition ease-in-out duration-300 hover:bg-[#0D1028]  text-slate-50">
+              <button className="font-roboto font-bold uppercase text-base lg:text-xl mt-10 px-6 py-3 lg:px-10 lg:py-5 bg-[#FF5202] transition ease-in-out duration-300 hover:bg-[#0D1028]  text-slate-50">
                 contact us
               </button>
             </div>
           </div>
         </div>
       </div>
-      <div className="service__section">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ea
-          voluptatum ducimus officiis, doloribus assumenda natus asperiores
-          aliquam? Animi repellendus nulla suscipit maiores, aspernatur itaque
-          excepturi corrupti! A, omnis sit autem est aspernatur corrupti id
-          repudiandae in nobis optio cumque magni eveniet dolorum repellat, iure
-          excepturi corporis. Eligendi voluptates porro perferendis soluta illo
-          aperiam dolorem cumque, velit recusandae distinctio nemo quasi ipsam.
-          Deleniti, natus! Consectetur id aspernatur, quae a, eaque ut ullam
-          modi rem dolores quod temporibus deserunt repellendus consequuntur est
-          alias minima cupiditate. Totam, quisquam repudiandae animi
-          necessitatibus ipsa praesentium est, fuga molestias eaque labore
-          reiciendis accusantium mollitia! Aspernatur sit nulla nihil, odit,
-          officiis voluptatum labore magnam maxime tempore, modi voluptatibus!
-          Ullam sint inventore, iure, sequi tempore dolore, rerum cupiditate
-          quae ex quisquam molestias excepturi. Optio eveniet harum voluptas
-          provident, dolorum facere iure ab soluta quod ut. Nam dolor quaerat
-          culpa voluptates, ex minus consequatur maiores voluptatibus nulla? A
-          corporis incidunt, provident tempora laborum commodi accusantium
-          perferendis natus officia voluptate error ducimus dignissimos maiores,
-          exercitationem non velit quam repudiandae. Magni dolores similique
-          eligendi atque veritatis deleniti at perferendis dolorem dolor
-          recusandae sapiente non sit fuga ducimus esse vero maiores sequi quod
-          quas quisquam voluptatibus, quos facere aut! Dolorum, ipsum
-          consequatur. Maxime nihil nulla eius nobis aliquam reiciendis
-          provident? Tenetur aspernatur repellat libero optio saepe? Optio
-          doloribus saepe sit reprehenderit modi hic dolorum consequatur
-          accusamus ipsa, quia soluta voluptatum quod fugiat. Possimus quis
-          laboriosam fugit dolor maiores dolore illo impedit est laudantium
-          voluptas id deleniti ipsa, doloribus modi alias vel itaque facilis
-          nobis ut! Officia ad non reiciendis laudantium quisquam sequi
-          nesciunt, recusandae necessitatibus. Inventore velit a cumque iusto
-          quas assumenda omnis suscipit alias, minus consectetur voluptatibus
-          ullam, itaque adipisci non nobis exercitationem libero asperiores
-          commodi ducimus nostrum modi vel provident. Hic ab provident velit
-          repellat cumque, reiciendis distinctio est?
-        </p>
+      <div className="service__section mt-20 mb-20">
+        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-6 lg:gap-x-6 gap-y-6">
+          <div className="item p-10 sm:p-8 md:p-6 lg:p-5 text-center transition ease-in-out duration-300 bg-[#0D1028] hover:bg-[#FF5202] text-white">
+            <div className="icon w-16 h-16 md:w-20 md:h-20 lg:w-32 lg:h-32 mx-auto mb-10">
+              <FaUsers className="w-full h-full" />
+            </div>
+            <h2 className="font-roboto font-bold  text-lg md:text-xl lg:text-2xl">
+              Skilled Lecturers
+            </h2>
+            <p className="font-roboto text-sm md:text-base lg:text-lg pt-2 pb-6">
+              The point of using Lorem Ipsum is that it has a more-or-less
+              normal distribution of letters, as opposed to using. Lorem ipsum
+              dolor sit, amet consectetur adipisicing elit. Nihil.
+            </p>
+            <Link
+              href={"#"}
+              className="font-roboto animation ease-in-out duration-300 uppercase text-base font-bold hover:text-[#0D1028] flex items-center justify-center"
+            >
+              read more <BiChevronRight className="w-8 h-8" />
+            </Link>
+          </div>
+          <div className="item p-10 sm:p-8 md:p-6 lg:p-5 text-center transition ease-in-out duration-300 bg-[#0D1028] hover:bg-[#FF5202] text-white">
+            <div className="icon w-16 h-16 md:w-20 md:h-20 lg:w-32 lg:h-32 mx-auto mb-10">
+              <FaUserGraduate className="w-full h-full" />
+            </div>
+            <h2 className="font-roboto font-bold  text-lg md:text-xl lg:text-2xl">
+              Scholarship Facility
+            </h2>
+            <p className="font-roboto text-sm md:text-base lg:text-lg pt-2 pb-6">
+              The point of using Lorem Ipsum is that it has a more-or-less
+              normal distribution of letters, as opposed to using. Lorem ipsum
+              dolor sit, amet consectetur adipisicing elit. Nihil.
+            </p>
+            <Link
+              href={"#"}
+              className="font-roboto animation ease-in-out duration-300 uppercase text-base font-bold hover:text-[#0D1028] flex items-center justify-center"
+            >
+              read more <BiChevronRight className="w-8 h-8" />
+            </Link>
+          </div>
+          <div className="item p-10 sm:p-8 md:p-6 lg:p-5 text-center transition ease-in-out duration-300 bg-[#0D1028] hover:bg-[#FF5202] text-white">
+            <div className="icon w-16 h-16 md:w-20 md:h-20 lg:w-32 lg:h-32 mx-auto mb-10">
+              <GiOpenBook className="w-full h-full" />
+            </div>
+            <h2 className="font-roboto font-bold text-lg md:text-xl lg:text-2xl">
+              Book Library & Store
+            </h2>
+            <p className="font-roboto text-sm md:text-base lg:text-lg pt-2 pb-6">
+              The point of using Lorem Ipsum is that it has a more-or-less
+              normal distribution of letters, as opposed to using. Lorem ipsum
+              dolor sit, amet consectetur adipisicing elit. Nihil.
+            </p>
+            <Link
+              href={"#"}
+              className="font-roboto animation ease-in-out duration-300 uppercase text-sm md:text-base lg:text-lg font-bold hover:text-[#0D1028] flex items-center justify-center"
+            >
+              read more <BiChevronRight className="w-8 h-8" />
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
